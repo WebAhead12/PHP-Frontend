4d862b788a5cd31f76e90baae0fb913e6d1a032e
Diabolica <scripter.Diabolica@gmail.com>
2021-12-14 16:03:39 +0200

    setup

---
+++ src/components/account/login/login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./login.module.css";
+
+import Stack from "@mui/material/Stack";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
+export default function Login() {
+  return;
+  <div></div>;
+}
