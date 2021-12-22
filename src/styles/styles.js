b310267fe1404c8c913ad71cacce30adbad046d3
Nida Abu Sneineh <nida.abusneineh@gmail.com>
2021-12-22 14:29:12 +0200

    Finished module component

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -17,5 +17,55 @@ const accountLogoStyle = {
   objectFit: "cover",
 };
 
-const exports = { accountLogoStyle };
+const moduleImage = {
+  position: "absolute",
+  width: "95%",
+  height: "95%",
+  transform: "translate(-50%, -50%)",
+  left: "50%",
+  top: "50%",
+  border: "0px",
+  pointerEvents: "none",
+};
+
+const moduleShortcutImage = {
+  position: "absolute",
+  width: "95%",
+  height: "70%",
+  left: "50%",
+  transform: "translateX(-50%)",
+  top: "2.5%",
+  border: "0px",
+  pointerEvents: "none",
+};
+
+const moduleText = {
+  position: "absolute",
+  margin: "0 0",
+  width: "95%",
+  height: "95%",
+  transform: "translate(-50%, -50%)",
+  left: "50%",
+  top: "50%",
+  textAlign: "start",
+  wordBreak: "break-word",
+  overflow: "hidden",
+  fontFamily: "Montbold",
+};
+
+const moduleShortcutText = {
+  position: "absolute",
+  height: "20%",
+  width: "95%",
+  margin: "0 0",
+  left: "50%",
+  transform: "translateX(-50%)",
+  top: "77.5%",
+  textAlign: "center",
+  wordBreak: "break-word",
+  overflow: "hidden",
+  fontFamily: "Montbold",
+};
+
+const exports = { accountLogoStyle, moduleImage, moduleShortcutImage, moduleText, moduleShortcutText };
 export default exports;
