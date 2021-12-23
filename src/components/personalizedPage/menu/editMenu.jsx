a8b54d06e4112bd2aa58e3c433c94716a4b3ac57
julio1703 <julio.elias22@gmail.com>
2021-12-23 16:11:21 +0200

    .

---
+++ src/components/personalizedPage/menu/editMenu.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import themes from "../../../styles/muiCustomThemes";
+
+import { ThemeProvider } from "@mui/material/styles";
+
+import { Stack, Button, CardContent, Card, Switch } from "@mui/material";
+
+function BuildEditMenu({ shortcutMode, setShortcutMode, imageCheck, setImageCheck, textCheck, setTextCheck }) {
+  return (
+    <Card>
+      <CardContent>
+        <Stack direction="column" space="2" alignContent="center">
+          <Stack sx={{ display: "block" }} direction="row" space="1" alignContent="center">
+            <span style={{ alignSelf: "center", color: "#fc6161" }}>{"Image"}</span>
+            <Switch
+              checked={imageCheck}
+              onChange={() => {
+                setImageCheck(!imageCheck);
+              }}
+            ></Switch>
+          </Stack>
+          {imageCheck ? (
+            <label style={{ width: "100%" }} htmlFor="contained-button-file">
+              <input accept="image/*" id="contained-button-file" type="file" style={{ display: "none" }} />
+              <Button variant="contained" sx={{ width: "100%" }} component="span">
+                Upload Image
+              </Button>
+            </label>
+          ) : null}
+          <Stack sx={{ display: "block" }} direction="row" space="1" alignContent="center">
+            <span style={{ alignSelf: "center", color: "#fc6161" }}>{"Text"}</span>
+            <Switch
+              checked={textCheck}
+              onChange={() => {
+                setTextCheck(!textCheck);
+              }}
+            ></Switch>
+          </Stack>
+          {textCheck ? (
+            <label style={{ width: "100%" }} htmlFor="contained-button-file">
+              <input id="contained-button-file" type="text" />
+            </label>
+          ) : null}
+          <Stack sx={{ display: "block" }} direction="row" space="1" alignContent="center">
+            <span style={{ alignSelf: "center", color: "#fc6161" }}>{"Shortcut"}</span>
+            <Switch
+              checked={shortcutMode}
+              onChange={() => {
+                setShortcutMode(!shortcutMode);
+              }}
+            ></Switch>
+          </Stack>
+          <Button>DELETE</Button>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+export default function editMenu({ editMenu, shortcutMode, setShortcutMode, imageCheck, setImageCheck, textCheck, setTextCheck }) {
+  console.log(editMenu);
+  return (
+    <>
+      {editMenu ? (
+        <BuildEditMenu
+          shortcutMode={shortcutMode}
+          setShortcutMode={setShortcutMode}
+          imageCheck={imageCheck}
+          setImageCheck={setImageCheck}
+          textCheck={textCheck}
+          setTextCheck={setTextCheck}
+        ></BuildEditMenu>
+      ) : null}
+    </>
+  );
+}
--- src/components/personalizedPage/menu/menu.jsx
+++ src/components/personalizedPage/menu/menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import themes from "../../../styles/muiCustomThemes";
 
-import { ThemeProvider, styled } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import { Card } from "@mui/material";
 
@@ -12,7 +12,7 @@ import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 import { Stack, Button, CardContent } from "@mui/material";
 
-function BuildMenu({ menuToggle }) {
+function BuildMenu({ menuToggle, setEditMenu, editMenu }) {
   return (
     <ThemeProvider theme={themes.menuTheme}>
       <Card>
@@ -20,26 +20,22 @@ function BuildMenu({ menuToggle }) {
           <Stack direction="column" spacing={2} alignContent="center">
             <Button variant="contained">Add Module</Button>
             <label style={{ width: "100%" }} htmlFor="contained-button-file">
-              <input
-                accept="image/*"
-                id="contained-button-file"
-                type="file"
-                style={{ display: "none" }}
-              />
-              <Button
-                variant="contained"
-                sx={{ width: "100%" }}
-                component="span"
-              >
+              <input accept="image/*" id="contained-button-file" type="file" style={{ display: "none" }} />
+              <Button variant="contained" sx={{ width: "100%" }} component="span">
                 Change Background
               </Button>
             </label>
-            <Button variant="contained">Edit Mode</Button>
+            <Button variant="contained" onClick={() => setEditMenu(!editMenu)}>
+              Edit Mode
+            </Button>
             <ThemeProvider theme={themes.menuToggleTheme}>
               <Button
                 variant="contained"
-                sx={{ padding: "clamp(5px,5%,10px) 0" }}
-                onClick={() => menuToggle(false)}
+                sx={{ padding: "clamp(5px,5%,20px) 0" }}
+                onClick={() => {
+                  setEditMenu(false);
+                  menuToggle(false);
+                }}
               >
                 <ArrowUpwardIcon sx={{ transform: "scale(1.34)" }} />
               </Button>
@@ -51,16 +47,12 @@ function BuildMenu({ menuToggle }) {
   );
 }
 
-export default function Menu({ menu, menuToggle }) {
+export default function Menu({ menu, menuToggle, editMenu, setEditMenu }) {
   return menu ? (
-    <BuildMenu menuToggle={menuToggle}></BuildMenu>
+    <BuildMenu menuToggle={menuToggle} editMenu={editMenu} setEditMenu={setEditMenu}></BuildMenu>
   ) : (
     <ThemeProvider theme={themes.menuToggleTheme}>
-      <Button
-        variant="contained"
-        sx={{ height: "7%", width: "75%" }}
-        onClick={() => menuToggle(true)}
-      >
+      <Button variant="contained" sx={{ height: "7%", width: "75%" }} onClick={() => menuToggle(true)}>
         <ArrowDownwardIcon sx={{ transform: "scale(1.34)" }} />
       </Button>
     </ThemeProvider>
